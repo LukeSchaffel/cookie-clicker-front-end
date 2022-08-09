5c43d148a0c0d20e5110937c0777308dff82fd39
LukeSchaffel <lukeschaffel@gmail.com>
2022-08-09 15:00:35 -0400

    start prices

--- src/pages/GameArea/GameArea.jsx
+++ src/pages/GameArea/GameArea.jsx
@@ -1,8 +1,9 @@
 import { updateProfile } from "../../services/profileService";
-
+import { buildings } from "../../services/buildings";
+import { useState, useEffect } from "react";
 
 const GameArea = ({ profile, refresh, localState, setLocalState }) => {
-  
+  const [buildingsData, setBuildingsData] = useState(buildings)
 
   const handleClick = () => {
     setLocalState(localState => {
@@ -12,19 +13,41 @@ const GameArea = ({ profile, refresh, localState, setLocalState }) => {
       }
     })
   }
-  console.log(localState._id);
 
   const handleSave = () => {
     updateProfile(localState._id, localState)
   }
+
+  const handlePurchase = (building) => {
+    const price = buildings[building].basePrice * Math.pow(1.5, localState[building])
+
+    if (localState.cookies >= price) {
+      localState[building]++
+      setLocalState((localState) => {
+        return {
+          ...localState,
+          cookies: cookies - price,
+        }
+      })
+    }
+  }
+
+  const setBuildingsPrices = () => {
+    console.log(buildingsData);
+  }
+
+  useEffect(() => {
+    setBuildingsPrices()
+  }, [])
 
   let { cursors, cookies, grandmas, id } = localState
   return (
     <>
       <h1>Game Area</h1>
       <h3>Cookies: {cookies}</h3>
-      <button className="cookie" onClick={()=> handleClick()}>This Is A Cookie</button>
+      <button className="cookie" onClick={() => handleClick()}>This Is A Cookie</button>
       <button className="save-btn" onClick={() => handleSave()}>SAVE</button>
+      <button className="purchase-cursor" onClick={() => { handlePurchase('cursors') }}>Purchase Cursor</button>
     </>
   );
 }
---
+++ src/services/buildings.js
@@ -0,0 +1,15 @@
+const buildings = {
+  cursors: {
+    basePrice: 15,
+    baseCPS: .1,
+    currentPrice: null
+  },
+  grandmas: {
+    basePrice: 100,
+    basCPS: 99,
+    currentPrice: null
+  }
+}
+
+
+export { buildings }
